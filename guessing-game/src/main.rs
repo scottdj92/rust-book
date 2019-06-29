c0f9c51e24c0b36ff2791a8858970fe598a175e5
Scott Jones <scottdj92@gmail.com>
2019-06-28 23:00:11 -0400

    initial commit

---
+++ guessing-game/src/main.rs
@@ -0,0 +1,34 @@
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
+fn main() {
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    loop {
+        println!("Please input your guess");
+
+        let mut guess = String::new();
+
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("too small"),
+            Ordering::Equal => {
+                println!("you win!");
+                break;
+            },
+            Ordering::Greater => println!("too big"),
+        }
+
+        println!("You guessed {}", guess);
+    }
+}
