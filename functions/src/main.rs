338c68c7c031702a283e08a30ab1ae410e8567d0
Scott Jones <scottdj92@gmail.com>
2019-07-02 22:37:55 -0400

    add basic for loop test

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -4,6 +4,8 @@ fn main() {
     another_function(5);
     flow_control(3);
     flow_control(7);
+
+    forLoop();
 }
 
 fn another_function(x: u32) {
@@ -16,6 +18,14 @@ fn flow_control(num: u32) {
         println!("condition was true");
     } else {
         println!("condition was false");
+    }
+}
+
+fn forLoop() {
+    let a = [10, 20, 30, 40, 50];
+
+    for element in a.iter() {
+        println!("The value is: {}", element);
     }
 }
 
